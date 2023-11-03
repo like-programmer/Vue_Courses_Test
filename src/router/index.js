6e72413ca3bb828987aec730bef163c578461e12
like-programmer <dashutkadevi@gmail.com>
2023-11-03 13:49:04 +0200

    init

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'login',
+  //   component: () => import('../pages/LoginPage.vue'),
+  // },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+// router.beforeEach((to, from, next) => {
+//   if (to.name !== 'login' && !localStorage.getItem('userData')) {
+//     next({ name: 'login' })
+//   } else {next()}
+// })
+
+export default router;
